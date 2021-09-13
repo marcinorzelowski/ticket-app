a86b887756961605dddd7a156e879b8f40ee3fbf
orzelke <marcin.orzelowski.stud@pw.edu.pl>
2021-09-13 18:00:06 +0200

    initial

---
+++ src/test/java/com/orzelowski/ticketapp/TicketappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orzelowski.ticketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
